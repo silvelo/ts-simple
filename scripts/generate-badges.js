8fd5f3d83051c3d50c5e9aeebfc406f6810ede17
Arturo Silvelo <arturo.silvelo@gmail.com>
2019-04-14 23:28:44 +0200

    Change script to work with travis  [ci skip]

--- scripts/generate-badges.js
+++ scripts/generate-badges.js
@@ -3,17 +3,20 @@
 const path = require('path');
 const replace = require('replace-in-file');
 const os = require('os');
-const folderName = process.env.LERNA_PACKAGE_NAME.replace('@ts-simple/', '');
-const directory = path.join(process.env.LERNA_ROOT_PATH, 'packages', folderName);
-const packageJson = path.join(directory, 'package.json');
-const readmeFile = path.join(directory, 'README.md');
-const package = require(packageJson);
 
+
+const parse = process.env.TRAVIS_BRANCH.replace('@ts-simple/', '');
+const folderName = parse.substr(0, parse.indexOf('@'))
+const directory = path.join(process.env.TRAVIS_BUILD_DIR, 'packages', folderName);
+const readmeFile = path.join(directory, 'README.md');
+console.log(`folderName: ${folderName}`);
+console.log(`directory: ${directory}`);
+console.log(`TRAVIS_BRANCH: ${process.env.TRAVIS_BRANCH}`);
 let coverallsString =
-  `[![Coverage Status](https://coveralls.io/repos/github/silvelo/ts-simple/badge.svg?branch=${process.env.LERNA_PACKAGE_NAME}@${package.version})](https://coveralls.io/github/silvelo/ts-simple?branch=${process.env.LERNA_PACKAGE_NAME}@${package.version})`
+  `[![Coverage Status](https://coveralls.io/repos/github/silvelo/ts-simple/badge.svg?branch=${process.env.TRAVIS_BRANCH})](https://coveralls.io/github/silvelo/ts-simple?branch=${process.env.TRAVIS_BRANCH})`
 
 let travisString =
-  `[![Build Status](https://travis-ci.org/silvelo/ts-simple.svg?branch=${process.env.LERNA_PACKAGE_NAME}@${package.version})](https://travis-ci.org/silvelo/ts-simple?branch=${process.env.LERNA_PACKAGE_NAME}@${package.version})`
+  `[![Build Status](https://travis-ci.org/silvelo/ts-simple.svg?branch=${process.env.TRAVIS_BRANCH})](https://travis-ci.org/silvelo/ts-simple?branch=${process.env.TRAVIS_BRANCH})`
 
 const tagKey = '<!-- Shield Tag -->';
 
