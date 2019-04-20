61c7670fa4d18aa19b28126caf1b9bc8296c7c37
Arturo Silvelo <arturo.silvelo@gmail.com>
2019-04-20 13:38:17 +0200

    Script autogenerate badges

--- scripts/generate-badges.js
+++ scripts/generate-badges.js
@@ -1,44 +1,56 @@
-//@ts-check
-
+// @ts-check
+const readlineSync = require('readline-sync');
+const Project = require("@lerna/project");
+const PackageGraph = require("@lerna/package-graph");
+const collectUpdates = require("@lerna/collect-updates");
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 const updateSection = require('update-section');
 
-const Input = require('prompt-input');
+async function main() {
+  const cwd = process.cwd();
+  const project = new Project(cwd);
+  const pkgs = await project.getPackages();
+  const graph = new PackageGraph(pkgs);
+  const opts = {
+    bump: "",
+    canary: false,
+    forcePublish: false,
+    ignoreChanges: [],
+    includeMergedTags: false,
+    since: null,
+  };
+  const changed = collectUpdates(pkgs, graph, { cwd }, opts);
+  changed.forEach(node => {
+    const readmeFile = path.join(project.packageParentDirs[0], node.name.replace('@ts-simple/', ''), 'README.md');
+    const updateVersion = readlineSync.question(`${node.name} => Current version: ${node.version}. New version: `);
+    const updateReadme = getBadges(node.name, updateVersion);
 
-const input  = new Input({
-    message: `Version fo ${process.env.LERNA_PACKAGE_NAME}`,
-    name: 'version',
-    validator: /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/
-  })
+    try {
+      const readmeContent = fs.readFileSync(readmeFile, 'utf-8');
+      const updated = updateSection(readmeContent, updateReadme, matchesStart, matchesEnd, true);
+      fs.writeFileSync(readmeFile, updated);
+    } catch (e) {
+      console.log(e.message);
+    }
 
-if (process.argv.length === 2) {
-  console.error('Expected at least one argument!');
-  process.exit(1);
+  });
 }
 
-input.render();
+function getBadges(name, version) {
+  const branch = `${name}@${version}`;
+
+  let coverallsString =
+    `[![Coverage Status](https://coveralls.io/repos/github/silvelo/ts-simple/badge.svg?branch=${branch})](https://coveralls.io/github/silvelo/ts-simple?branch=${branch})`
+
+  let travisString =
+    `[![Build Status](https://travis-ci.org/silvelo/ts-simple.svg?branch=${branch})](https://travis-ci.org/silvelo/ts-simple?branch=${branch})`
+  
+  return `<!-- START SHIELD -->${os.EOL}${travisString}${os.EOL}${coverallsString}${os.EOL}<!-- END SHIELD -->`;
+}
 
 
-
-const version = process.argv[2];
-const PACKAGE_NAME = process.env.LERNA_PACKAGE_NAME;
-const ROOT_PATH = process.env.LERNA_ROOT_PATH;
-const folderName = PACKAGE_NAME.replace('@ts-simple/', '');
-const directory = path.join(ROOT_PATH, 'packages', folderName);
-const readmeFile = path.join(directory, 'README.md');
-const branch = `${PACKAGE_NAME}@${version}`;
-
-
-let coverallsString =
-  `[![Coverage Status](https://coveralls.io/repos/github/silvelo/ts-simple/badge.svg?branch=${branch})](https://coveralls.io/github/silvelo/ts-simple?branch=${branch})`
-
-let travisString =
-  `[![Build Status](https://travis-ci.org/silvelo/ts-simple.svg?branch=${branch})](https://travis-ci.org/silvelo/ts-simple?branch=${branch})`
-
-
-const update = `<!-- START SHIELD -->${os.EOL}${travisString}${os.EOL}${coverallsString}${os.EOL}<!-- END SHIELD -->`;
 
 function matchesStart(line) {
   return (/<!-- START SHIELD -->/).test(line);
@@ -48,12 +60,5 @@ function matchesEnd(line) {
   return (/<!-- END SHIELD -->/).test(line);
 }
 
-try {
-  const readmeContent = fs.readFileSync(readmeFile, 'utf-8');
-  const updated = updateSection(readmeContent, update, matchesStart, matchesEnd, true);
-  fs.writeFileSync(readmeFile, updated);
-} catch (e) {
-  console.log(e.message);
-}
 
-
+main();
